75fb2c4bee27db771b1ea396ea4685d9592bd53f
AndreMP93 <andremirandap93@gmail.com>
2019-05-31 17:35:34 -0300

    Update test_core.py
    Teste Fatorial

--- test/test_core.py
+++ test/test_core.py
@@ -35,6 +35,10 @@ class TestMethods(unittest.TestCase):
     def test_avg(self):
         operation = Operations([1,2,3,4], False)
         self.assertAlmostEqual(operation.avg(), 2.5, msg='Incorrect result')
+        
+    def test_fatorial(self):
+        operation = Operations(5)
+        self.assertAlmostEqual(operation.fatorial(), 120, msg='Incorrect result')    
 
 if __name__ == '__main__':
     unittest.main()
